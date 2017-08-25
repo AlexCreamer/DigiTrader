064c306e6f10f53614cb9eab84127b17500cd36b
alexcreamer <carebear@nili.ca>
2017-08-24 23:55:07 -0300

    Including pk in urls.py, included block body and block css to templates

--- Credit_Ledger/urls.py
+++ Credit_Ledger/urls.py
@@ -10,7 +10,7 @@ from . import views
 urlpatterns = [
     # ex: /credits/
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^account_id/([0-9]+)/$', views.AccountDetail.as_view(), name="account_details" ),
+    url(r'^account_id/(?P<pk>[0-9]+)/$', views.AccountDetail.as_view(), name="account_details" ),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
