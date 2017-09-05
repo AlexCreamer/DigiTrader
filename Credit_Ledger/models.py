a285ddc5e2b8a27866b9779ae321a99e2b4ec407
alexcreamer <carebear@nili.ca>
2017-09-04 23:55:43 -0300

    Removed User class from models

--- Credit_Ledger/models.py
+++ Credit_Ledger/models.py
@@ -2,13 +2,8 @@ from django.db import models
 from decimal import Decimal
 from django.contrib.auth.models import User
 
-
 # Create your models here.
 class Account(models.Model):
     balance = models.DecimalField(max_digits=30, decimal_places=6, default=Decimal(0.000000))
     account_type = models.CharField(max_length=100)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-class User(models.Model):
-    username = models.CharField(max_length=200)
-    account_id = models.ForeignKey(Account)
