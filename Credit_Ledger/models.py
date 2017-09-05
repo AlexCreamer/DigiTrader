97ef8b837c1896d70602bb5f74e3bf614b0880de
alexcreamer <carebear@nili.ca>
2017-09-04 23:40:04 -0300

    Changed models and custom_auth to use User object in Account model instead of User id

--- Credit_Ledger/models.py
+++ Credit_Ledger/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from decimal import Decimal
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Account(models.Model):
     balance = models.DecimalField(max_digits=30, decimal_places=6, default=Decimal(0.000000))
     account_type = models.CharField(max_length=100)
-    user_id = models.IntegerField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 class User(models.Model):
     username = models.CharField(max_length=200)
--- Credit_Ledger/signals/custom_auth.py
+++ Credit_Ledger/signals/custom_auth.py
@@ -1,6 +1,8 @@
 from allauth.account.signals import user_signed_up
+from ..models import Account
+
 
 def add_account(request, user, **kwargs):
-    print("testing")
+    Account.objects.create(balance=0.0, account_type="regular", user=user) 
     print(user)
 user_signed_up.connect(add_account)
