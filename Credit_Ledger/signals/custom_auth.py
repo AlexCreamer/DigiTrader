ac52edd03a78dd49d0238c760587fd1230b7af38
alexcreamer <carebear@nili.ca>
2017-09-04 21:53:46 -0300

    Working signals implementation

--- Credit_Ledger/signals/custom_auth.py
+++ Credit_Ledger/signals/custom_auth.py
@@ -1,6 +1,6 @@
 from allauth.account.signals import user_signed_up
 
-def add_account(request, user):
+def add_account(request, user, **kwargs):
     print("testing")
     print(user)
 user_signed_up.connect(add_account)
