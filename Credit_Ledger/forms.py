29c8195b69dfc07f4f52f0529272673b95f9736a
alexcreamer <carebear@nili.ca>
2017-09-12 22:43:12 -0300

    Added extra textbox specifications

--- Credit_Ledger/forms.py
+++ Credit_Ledger/forms.py
@@ -2,9 +2,9 @@ from django.contrib.auth.models import User
 from django import forms
 
 class UserForm(forms.Form):
-    pk = forms.IntegerField(label='User to trade with')
-    amount = forms.IntegerField(label='Amount to trade')
+    pk = forms.IntegerField(label='User to trade with', widget=forms.Textarea)
+    amount = forms.IntegerField(label='Amount to trade', widget=forms.Textarea)
     
 class GrantForm(forms.Form):
-    pk = forms.IntegerField(label='User to grants credits to', widget="Textarea")
-    amount = forms.IntegerField(label='Amount to grant', widget="Textarea")
+    pk = forms.IntegerField(label='User to grants credits to', widget=forms.Textarea)
+    amount = forms.IntegerField(label='Amount to grant', widget=forms.Textarea)
